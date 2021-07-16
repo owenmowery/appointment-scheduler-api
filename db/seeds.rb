d0094395ae7d3c9a8811de7027fd82544a60bea3
Owen Mowery <owenmowery99@gmail.com>
2021-07-16 17:07:46 -0400

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.destroy_all
+
+Admin.create(
+    name: 'Owen Mowery',
+    business_name: 'Nitrous Sky',
+    email: 'owenmowery99@gmail.com',
+    password_digest: 'password'
+)
